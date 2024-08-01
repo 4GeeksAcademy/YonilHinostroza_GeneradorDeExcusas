4f3791fe07f2ea3749aa5222431f1f018ed1c404
yonilhp <55214610+yonilhp@users.noreply.github.com>
2024-08-01 19:50:57 +0000

    Se hizo cambios en javaScript y html

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whIndex = Math.floor(Math.random() * who.length);
+  let actIndex = Math.floor(Math.random() * action.length);
+  let whaIndex = Math.floor(Math.random() * what.length);
+  let wheIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whIndex] +
+    " " +
+    action[actIndex] +
+    "" +
+    what[whaIndex] +
+    " " +
+    when[wheIndex]
+  );
 };
